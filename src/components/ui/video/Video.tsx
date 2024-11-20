0e5d5a5fef66eaccf24402d7c136d17bbed06a6b
Abhay <abhaysharma.developer@gmail.com>
2024-11-20 12:51:03 +0530

    Basic p2p connection using websockets and webrtc

--- src/components/ui/video/Video.tsx
+++ src/components/ui/video/Video.tsx
@@ -10,13 +10,15 @@ const Video = () => {
     const localStreamRef = useRef<MediaStream>();
     const remoteStreamRef = useRef<MediaStream>();
     const peerConnection = useRef<RTCPeerConnection | null>();
-    const [socket, setSocket] = useState<WebSocket>()
+    // const [socket, setSocket] = useState<WebSocket>()
+    const socketRef = useRef<WebSocket | null>(null)
 
 
     // open media devices (ask for user permissions)
     const openMediaDevices = async (constraints: Constraints) => {
         if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
             alert('Your browser does not support WebRTC.');
+            return
         }
         return await navigator.mediaDevices.getUserMedia(constraints);
     }
@@ -29,9 +31,11 @@ const Video = () => {
             console.log('Got MediaStream:', localStreamRef.current);
 
             const videoElement = document.querySelector('video#localVideo') as HTMLVideoElement;
-            if (videoElement) {
+            if (videoElement && localStreamRef.current) {
                 videoElement.srcObject = localStreamRef.current;
             }
+
+            return;
 
         } catch (error: unknown) {
             console.error('Error accessing media devices.', error);
@@ -40,81 +44,117 @@ const Video = () => {
 
     // Create WS connection
     const wsConnection = () => {
-        const socket = new WebSocket('http://localhost:8080');
+        if (!socketRef.current) {
+            socketRef.current = new WebSocket('ws://localhost:8080');
 
-        socket.onopen = () => {
-            console.log('Conneciton made with the server')
-        }
+            // socket on open
+            socketRef.current.onopen = () => {
+                console.log('Conneciton made with the server')
 
-        socket.onmessage = async (message) => {
-            const reader = await new Response(message.data).text();
-            const data = JSON.parse(reader);
-            console.log(data);
-            // const data = JSON.parse(message.data)
-
-            if (data.offer) {
-                await peerConnection.current?.setRemoteDescription(new RTCSessionDescription(data.offer));
-                const answer = await peerConnection.current?.createAnswer();
-                await peerConnection.current?.setLocalDescription(answer);
-                socket.send(JSON.stringify({ answer }));
-            } else if (data.answer) {
-                await peerConnection.current?.setRemoteDescription(new RTCSessionDescription(data.answer));
-            } else if (data.candidate) {
-                await peerConnection.current?.addIceCandidate(new RTCIceCandidate(data.candidate));
             }
+
+            // socket on message
+            socketRef.current.onmessage = async (message) => {
+
+                const data = JSON.parse(await message.data.text());
+                console.log(data);
+
+                if (data.type == 'offer' && peerConnection.current) {
+                    console.log('offer recieved')
+                    await peerConnection.current.setRemoteDescription(new RTCSessionDescription(data));
+                    const answer = await peerConnection.current.createAnswer();
+                    await peerConnection.current.setLocalDescription(answer);
+                    socketRef.current!.send(JSON.stringify(answer));
+                } else if (data.type == 'answer' && peerConnection.current) {
+                    console.log('answer')
+                    console.log(peerConnection.current.signalingState);
+                    await peerConnection.current.setRemoteDescription(new RTCSessionDescription(data));
+                } else if (data.candidate && peerConnection.current) {
+                    console.log('candidate')
+                    await peerConnection.current.addIceCandidate(new RTCIceCandidate(data));
+                }
+            }
+
+            // socket on close
+            socketRef.current.onclose = () => {
+                const videoElement = document.querySelector('video#localVideo') as HTMLVideoElement;
+                if (videoElement) videoElement.srcObject = null;
+
+                const remoteVideoElement = document.querySelector('video#remoteVideo') as HTMLVideoElement;
+                if (remoteVideoElement) remoteVideoElement.srcObject = null;
+
+                console.log("WebSocket closed");
+                // setTimeout(wsConnection, 2000); // Reconnect after 2 seconds
+            };
+
+
+        }
+    }
+
+    // Set peer connection
+    const setPeerCOnnection = async () => {
+        const remoteVideo = document.querySelector('video#remoteVideo') as HTMLVideoElement;
+
+        const configuration = {
+            iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+        };
+        peerConnection.current = new RTCPeerConnection(configuration);
+
+        if (localStreamRef.current) {
+            console.log('true')
+            localStreamRef.current.getTracks().forEach(track => peerConnection.current?.addTrack(track, localStreamRef.current as MediaStream));
         }
 
-        setSocket(socket)
+        peerConnection.current.ontrack = (event) => {
+            console.log('tracked')
+            // Add remote tracks to the remoteStream
+            if (!remoteStreamRef.current) {
+                remoteStreamRef.current = new MediaStream();
+                remoteVideo.srcObject = remoteStreamRef.current;
+            }
+            remoteStreamRef.current.addTrack(event.track);
+        };
+
+        peerConnection.current.onicecandidate = (event) => {
+            console.log('ice canditate')
+            if (event.candidate) {
+                // Send the candidate to the remote peer through signaling
+                if (socketRef.current) {
+                    socketRef.current.send(JSON.stringify(event.candidate));
+                }
+            }
+        };
     }
 
     // handle click 
     const handleClick = async () => {
-        // const button = document.getElementById('startCall')
-
-        // button.addEventListener('click', async () => {
-        console.log('button clickevd')
-        const offer = await peerConnection.current?.createOffer();
-        await peerConnection.current?.setLocalDescription(offer);
-        if (socket) {
-            socket.send(JSON.stringify({ offer }));
+        console.log('button clicked')
+        if (peerConnection.current) {
+            const offer = await peerConnection.current.createOffer();
+            await peerConnection.current.setLocalDescription(offer);
+            if (socketRef.current) {
+                console.log('data sent')
+                console.log(offer)
+                socketRef.current.send(JSON.stringify(offer));
+            }
         }
-        // });
     }
 
     useEffect(() => {
-        const remoteVideo = document.querySelector('video#remoteVideo') as HTMLVideoElement;
+        getLocalStream().then(() => {
+            wsConnection();
+            setPeerCOnnection()
+        });
 
-        getLocalStream();
-        wsConnection()
-        try {
-            const configuration = {
-                iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
-            };
-            peerConnection.current = new RTCPeerConnection(configuration);
 
-            localStreamRef.current?.getTracks().forEach(track => peerConnection.current?.addTrack(track, localStreamRef.current!));
 
-            peerConnection.current.ontrack = (event) => {
-                // Add remote tracks to the remoteStream
-                if (!remoteStreamRef.current) {
-                    remoteStreamRef.current = new MediaStream();
-                    remoteVideo.srcObject = remoteStreamRef.current;
-                }
-                remoteStreamRef.current.addTrack(event.track);
-            };
-
-            peerConnection.current.onicecandidate = (event) => {
-                if (event.candidate) {
-                    // Send the candidate to the remote peer through signaling
-                    if (socket) {
-                        socket.send(JSON.stringify({ candidate: event.candidate }));
-                    }
-                }
-            };
-
-        } catch (error) {
-
-        }
+        // return () => {
+        //     if (socketRef.current) {
+        //         socketRef.current.close();
+        //     }
+        //     peerConnection.current?.close();
+        //     localStreamRef.current?.getTracks().forEach(track => track.stop());
+        // };
     }, [])
 
 
